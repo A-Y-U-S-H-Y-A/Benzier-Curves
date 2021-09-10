da86db6a0792b3cd20719a9d410859f6c3143b91
A-Y-U-S-H-Y-A <ayushya.shah1201@gmail.com>
2021-09-10 21:01:43 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Benzier-Curves
+# Bezier-Curves
+
+Foobar is a Python Program using PIL to create connected Bezier Curves to create a colorful abstract design
+
+## Requirements
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the requirements (PIL)
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+Go to the newly downloaded directory and run
+```bash
+python main.py
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ main.py
@@ -0,0 +1,63 @@
+import random
+import math
+from PIL import Image, ImageDraw
+h = int(input('Enter image height: '))
+w = int(input('Enter image width: '))
+n = int(input('Enter number of curves: '))
+img = Image.new("RGB", (w,h), "white")
+draw = ImageDraw.Draw(img)
+p1 = []
+p1.append(random.randint(0,w))
+p1.append(random.randint(0,h))
+p1 = tuple(p1)
+pp = [-99,-99] 
+for j in range (n):
+    p2 = []
+    p2.append(random.randint(0,w))
+    p2.append(random.randint(0,h))
+    p2 = tuple(p2)
+    p3 = []
+    p3.append(random.randint(0,w))
+    p3.append(random.randint(0,h))
+    p3 = tuple(p3)
+    for i in range(10000):
+        i/=100
+        x = math.floor((i * (p2[0] - p1[0]) / 100) + p1[0])
+        y = math.floor((i * (p2[1] - p1[1]) / 100) + p1[1])
+        sp1 = []
+        sp1.append(x)
+        sp1.append(y)
+        sp1=tuple(sp1)
+        x = math.floor((i * (p3[0] - p2[0]) / 100) + p2[0])
+        y = math.floor((i * (p3[1] - p2[1]) / 100) + p2[1])
+        sp2 = []
+        sp2.append(x)
+        sp2.append(y)
+        sp2=tuple(sp2)
+        x = math.floor((i * (sp2[0] - sp1[0]) / 100) + sp1[0])
+        y = math.floor((i * (sp2[1] - sp1[1]) / 100) + sp1[1])
+        mp = []
+        mp.append(x)
+        mp.append(y)
+        mp=tuple(mp)
+        if(tuple(pp) != tuple(mp)):
+            if j == 0:
+                draw.point(mp, fill="black")
+                pp=list(mp)
+            else:
+                lc = []
+                lc.append(tuple(pp))
+                lc.append(mp)
+                draw.line(lc,fill="black")
+                pp = list(mp)
+            
+    p1 = p3
+
+for i in range(1000):
+    for j in range(1000):
+        if (img.getpixel((i,j)) == (255, 255, 255)):
+            ImageDraw.floodfill(img,(i,j),(random.randint(1,244),random.randint(1,244),random.randint(1,244)),thresh=50)
+        else:
+            continue
+        
+img.save("design9.png")
